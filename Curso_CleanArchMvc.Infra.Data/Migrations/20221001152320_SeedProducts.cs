085dd637719fb7434d4cf7962f99b3499f65e154
LukeDaniel16 <lucasfelepe@gmail.com>
2022-10-01 12:31:49 -0300

    Finalizando inicialmente a camada de Infra.data e migrations.

---
+++ Curso_CleanArchMvc.Infra.Data/Migrations/20221001152320_SeedProducts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, ImagePath, CategoryId) " +
+                "VALUES('Caderno espiral', 'Caderno espiral 100 folhas', 7.45, 50, 'caderno1.jpg', 1)");
+            
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, ImagePath, CategoryId) " +
+                "VALUES('Estojo escolar', 'Estojo escolar simples cinza', 4.50, 35, 'estojo1.jpg', 1)");
+            
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, ImagePath, CategoryId) " +
+                "VALUES('Calculadora', 'Calculadora científica', 25.00, 25, 'calculadora1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
